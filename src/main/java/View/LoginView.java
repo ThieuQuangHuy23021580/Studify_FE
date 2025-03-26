423acdf377a7ac344aee873db6701c7e01a48255
ThieuQuangHuy23021580 <160751958+ThieuQuangHuy23021580@users.noreply.github.com>
2025-03-27 04:46:06 +0700

    DashBoard_fix

--- src/main/java/View/LoginView.java
+++ src/main/java/View/LoginView.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class LoginView {
     public void Start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/controller/FXML/LoginView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/controller/FXML/DashBoardView.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
--- src/main/java/controller/DashBoardController.java
+++ src/main/java/controller/DashBoardController.java
@@ -1,6 +1,18 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+
+
 
 public class DashBoardController {
+    @FXML
+    private AnchorPane pomodoroFixAnchorPane;
+    @FXML
+    private AnchorPane pomodoroAnchorPane;
+    @FXML
+    public void initialize() {
+        pomodoroFixAnchorPane.setManaged(false);
+    }
 }
