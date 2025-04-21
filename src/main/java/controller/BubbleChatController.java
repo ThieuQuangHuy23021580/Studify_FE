64b6bc475e27e6a7fbb606825d1d267a41702037
manhdua1 <manhvu20012005@gmail.com>
2025-04-21 22:56:09 +0700

    Add BubbleChatController

--- src/main/java/controller/BubbleChatController.java
+++ src/main/java/controller/BubbleChatController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class BubbleChatController {
+    @FXML
+    private Label messageLabel;
+
+    public void setMessage(String message) {
+        messageLabel.setText(message);
+    }
 }
